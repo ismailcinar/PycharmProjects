c2e284df4646694de94375f6b99e191ad0fbeb69
İsmail Çınar <ismailcinar99@gmail.com>
2021-10-30 17:24:23 +0300

    exercise

---
+++ exercise/game_data.py
@@ -0,0 +1,29 @@
+data = [
+
+
+ {
+     'name':'Instagram',
+     'follower_count': 346,
+     'description':'Social media platform',
+     'country':'United States'
+ },
+    {
+        'name': 'Instagram',
+        'follower_count': 322,
+        'description': 'Footballer',
+        'country': ' Portugal',
+    },
+    {
+        'name': 'ariana',
+        'follower_count': 34536,
+        'description': 'Social media platform',
+        'country': 'Turkey',
+    },
+    {
+        'name': 'cr',
+        'follower_count': 11,
+        'description': 'Footballer',
+        'country': 'United States',
+    }
+
+]
